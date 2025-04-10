295da1d3f136d3271d7649bd03fc21568e10e78c
Yanatchara JERAJA <yanatchara.j@ku.th>
2025-04-10 15:32:24 +0700

    add data type

---
+++ lib/data.ts
@@ -0,0 +1,16 @@
+// lib/data.ts
+import { Item } from '@/lib/types'; // Import the type
+
+export const initialItems: Item[] = [
+  { type: 'Fruit', name: 'Apple' },
+  { type: 'Vegetable', name: 'Broccoli' },
+  { type: 'Vegetable', name: 'Mushroom' },
+  { type: 'Fruit', name: 'Banana' },
+  { type: 'Vegetable', name: 'Tomato' },
+  { type: 'Fruit', name: 'Orange' },
+  { type: 'Fruit', name: 'Mango' },
+  { type: 'Fruit', name: 'Pineapple' },
+  { type: 'Vegetable', name: 'Cucumber' },
+  { type: 'Fruit', name: 'Watermelon' },
+  { type: 'Vegetable', name: 'Carrot' },
+];
